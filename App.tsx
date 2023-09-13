88845bd61f563198a63e3f04efd1a06fc16fa1cb
VIIIbyuL <myjjk03@gmail.com>
2023-09-13 17:51:32 -0400

    starting + error

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
 
+import Scanner from "./src/pages/Scanner";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
       <StatusBar style="auto" />
-      <Button title="chat is this real" />
+      {/* <Button title="chat is this real" /> */}
+
+      {/* QR camera component */}
+      <Scanner />
     </View>
   );
 }
