00cf2508b6ecfc8f1f098525fefbe08a2a04e2df
VIIIbyuL <myjjk03@gmail.com>
2023-12-30 22:47:27 -0500

    Add Clerk integration and update dependencies

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
+import { ClerkProvider } from "@clerk/clerk-react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Constants, { ExpoConfig } from "expo-constants";
 import React, { useEffect } from "react";
 
 import HomePage from "./src/pages/HomePage";
 import ProfilePage from "./src/pages/ProfilePage";
 import SearchPage from "./src/pages/SearchPage";
-import { setupDatabaseAsync } from "./src/services/database"; // Make sure this import path is correct
+import { setupDatabaseAsync } from "./src/services/database";
+
+interface CustomManifestExtra extends ExpoConfig {
+  CLERK_PUBLISHABLE_KEY?: string;
+}
 
 export default function App() {
   const stack = createStackNavigator();
+  const publishableKey = (Constants.manifest2?.extra as CustomManifestExtra)
+    ?.CLERK_PUBLISHABLE_KEY;
 
   useEffect(() => {
     async function prepareApp() {
@@ -20,13 +28,15 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen name="HomePage" component={HomePage} />
-        <stack.Screen name="SearchPage" component={SearchPage} />
-        <stack.Screen name="ProfilePage" component={ProfilePage} />
-        {/* Stack screen for other screens */}
-      </stack.Navigator>
-    </NavigationContainer>
+    <ClerkProvider publishableKey={publishableKey}>
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen name="HomePage" component={HomePage} />
+          <stack.Screen name="SearchPage" component={SearchPage} />
+          <stack.Screen name="ProfilePage" component={ProfilePage} />
+          {/* Stack screen for other screens */}
+        </stack.Navigator>
+      </NavigationContainer>
+    </ClerkProvider>
   );
 }
---
+++ app.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "QRvoyant",
+  version: "1.0.0",
+  extra: {
+    clerkPublishableKey:
+      "pk_test_dGhvcm91Z2gtc25ha2UtNTQuY2xlcmsuYWNjb3VudHMuZGV2JA",
+  },
+};
